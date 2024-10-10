a44ed04c858c0856caca885351d9b2a1cad27d99
Avik Sarkar <aviksarkar89@users.noreply.github.com>
2024-10-09 22:51:00 -0400

    Update SimpletronMachine.java

--- src/main/java/com/cisc/simpletron/SimpletronMachine.java
+++ src/main/java/com/cisc/simpletron/SimpletronMachine.java
@@ -9,17 +9,20 @@ package com.cisc.simpletron;
 import com.cisc.simpletron.processor.SimpletronProcessor;
 import com.cisc.simpletron.scanner.ScannerHelper;
 
+/**
+ * The main class where the Simpletron Machine runs.
+ */
 public class SimpletronMachine {
 
-    private static final SimpletronProcessor processor = new SimpletronProcessor(100, new ScannerHelper());
+    private static final SimpletronProcessor PROCESSOR = new SimpletronProcessor(100, new ScannerHelper());
 
     public static void main(String[] args) {
         printWelcomeMessage();
-        processor.process();
+        PROCESSOR.process();
     }
 
     private static void printWelcomeMessage() {
-        System.out.println("""
+        String welcomeMessage = """
             *** Welcome to Simpletron! ***
             *** Please enter your program one instruction ***
             *** (or data word) at a time. I will type the ***
@@ -27,6 +30,7 @@ public class SimpletronMachine {
             *** number and a question mark (?). You then  ***
             *** type the word for that location. Enter    ***
             *** -99999 to stop entering your program.     ***
-            """);
+            """;
+        System.out.println(welcomeMessage);
     }
 }
