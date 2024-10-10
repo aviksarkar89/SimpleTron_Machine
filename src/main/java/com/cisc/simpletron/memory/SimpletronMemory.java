a80fc34495bd27ed47b7308caffa0768c70ed345
Avik Sarkar <aviksarkar89@users.noreply.github.com>
2024-10-09 23:13:19 -0400

    SimpletronMemory.java

--- src/main/java/com/cisc/simpletron/memory/SimpletronMemory.java
+++ src/main/java/com/cisc/simpletron/memory/SimpletronMemory.java
@@ -1,18 +1,12 @@
-package com.cisc.simpletron.memory;
-
 /**
- * The class work as memory for Simpletron machine
- * It includes the memory structure as an integer array, and size
- * The memory size could be specified by the constructor at the time of initialization
- * <p>
- * Providing three helper methods:
- * 1) save value into target address
- * 2) get value at a specific address
- * 3) get memory size
- *
- * @author Feifan Wu
+ * The class defined serves as the memory for the Simpletron, where the user defines
+ the mem size parameters and int array etc. 
+ * @author Avik Sarkar
  * @version 3.0
  */
+
+package com.cisc.simpletron.memory;
+
 public class SimpletronMemory {
     private final int[] memory;
     private final int size;
